08068c6300b49f3886d85b917bfe9cd08c58b27e
Helolara <heloise.lara@escola.pr.gov.br>
2024-08-30 12:06:29 -0300

    subir api 04

--- api.js
+++ api.js
@@ -1,85 +1,85 @@
-//consultar tarefa por id
-//atualizar o status de uma tarefa
-
-//1 requires
-
+//atualizar o texto da task 
 const express = require ('express');
-const mysql = require ('mysql2');
-const cors = require ('cors');
+const mysql = require('mysql2');
+const cors = require('cors');
 
 const mysql_config = require ('./inc/mysql_config');
-const functions = require ('./inc/fumctions');
+const functions = require('./inc/functions');
+const { json } = require('express');
 
-//2 criação de duas constantes para definição da disponilibidade da api e da versão da api
-const api_availability = true;
-const api_version = '1.0.0';
+//2 criação de duas constantes para a definição da disponibilidade da api e da versão da api
+const API_AVAILABILITY = true;
+const API_VERSION = "4.0.0";
 
-//3 iniciar o server
-const app = express();
-app.listen(3000, ()=>{
-    console.log("API está executando")
+const app = express()
+app.listen(3000,()=>{
+    console.log("APi esta executando")
 })
 
-//4 checar se API está disponível 
-app.use((req,res, next)=>{
-    if (api_availability){
+
+
+app.use((req,res,next)=>{
+    if(API_AVAILABILITY){
         next();
     }else{
-        res.json(functions.response('atenção', 'API está em manutenção. Sinto muito',0,null))
+        res.json(functions.response("Atenção", "API esta em manutenção. Sinto muito",0,null))
     }
 })
 
-//5 mysql connection
-const connection = mysql.createConnection(mysql_config)
 
-//6 cors
+
+const connection = mysql.createConnection(mysql_config);
+
+
+
 app.use(cors());
 
-//tratamento dos posts params
+//tratamento dos posts params 
 app.use(json());
-//instrução que pede que o express trate os dados como json
+//instrução que pede que o express trate os dados com json
 
 app.use(express.urlencoded({extended: true}));
-//quando é enviado um pedido através do método post, os dados enviados
-//através 
 
-//7 rotas
-//rota inicial que vai dizer que a API está disponível 
-app.get('/', (req,res)=>{
-    res.json(functions.response('sucesso', 'API está rodando',0,null))
+//quando é enviado um pedido atraves do metodo post, os dados enviados atráves de um formulário podem ser interpretados
+//SEM ESSES DOIS MIDLEWARE NÃO SERIA POSSIVEL BUSCAR OS PARAMETROS
+
+//ROTAS
+//rota de entrada
+app.get('/',(req,res)=>{
+    res.json(functions.response('sucesso','api esta rodando',0,null))
 })
 
-//9 rota para pegar todas as tarefas
+//rota para pegar todas as tarefas
 app.get('/tasks', (req,res)=>{
-    connection.query('SELECT * FROM tasks',(err, rows))
+     connection.query("SELECT * FROM tasks",(err,rows))
 })
 
-//10 rotas para pegar a task pelo id
-app.get('/tasks/:id', (req,res)=>{
+//rota para pegar a task pelo id
+app.get("/tasks/:id",(req,res)=>{
     const id = req.params.id;
-    connection.query('SELECT * FROM tasks WHERE id=?',[id],(err,rows)=>{
-        if(!err){
+    connection.query('SELECT *FROM tasks WERE id=?',(id),(err,rows)=>{
+        if(err){
             //devolver os dados da task
             if(rows.lenght>0){
-                res.json(functions.response('Sucesso','Sucesso na pesquisa',rows,length,rows))
+                res.json(functions.response("Sucesso","Sucesso na pesquisa",rows.lenght,rows))
             }else{
-                res.json(functions.response('Atenção','Não foi possível encontrar a task solicitada',0,null))
+                res.json(functions.response('Atenção', 'Não foi possivel encontrar a task solicitada',0,null))
             }
         }
         else{
-            res.json(functions.response('Erro',err.message,0,null))
+            res.json(functions.response("error",err.message,0,null))
         }
     })
 })
 
-//11 rota para atualizar o status de uma task - método put
-app.put('/tasks/:id/status/:status',(req,res)=>{
+//rotas para atualizar o status de uma task  metodo put 
+app.put('/tasks/:id status/ status',(req,res)=>{
     const id = req.params.id;
     const status = req.params.status;
-    connection.query('UPDATE tasks SET status =? WHERE id=?',[status,id],(err,rows)=>{
-        if(!err){
+    connection.query('UPDATE tasks SET status =? WHERE id =?  ',(status,id),(err,rows)=>{
+        if (!err){
             if(rows.affectedRows>0){
-                res.json(functions.response('Sucesso','Sucesso na alteração do status',rows.affectedRows,null))
+                res.json(functions.response('Sucesso', 'Sucesso na alteração do status',rows.affectedRows,null))
             }
             else{
                 res.json(functions.response('Atenção','Task não encontrada',0,null))
@@ -92,12 +92,12 @@ app.put('/tasks/:id/status/:status',(req,res)=>{
 })
 
 //rota para deletar uma tarefa
-app.delete('/tasks/:id/delete',(req,res)=>{
+app.delete('/tasks/:id/delete', (req,res)=>{
     const id = req.params.id;
-    connection.query('DELETE FROM tasks WHERE id=?',[id],(err,rows)=>{
+    connection.query('DELETE FROM tasks WERE id=?',[id],(err,rows)=>{
         if(!err){
-            if(rows.affectedRows){
-                res.json(functions.response('Sucesso','Task deletada',rows.affectedRows,null))
+            if(rows.affectedRows>0){
+                res.json(functions.response('Sucesso','Tasks deletada', rows.affectedRows,null))
             }
             else{
                 res.json(functions.response('Atenção','Task não encontrada',0,null))
@@ -109,33 +109,75 @@ app.delete('/tasks/:id/delete',(req,res)=>{
     })
 })
 
+
 //rota para inserir uma nova task
 app.put('/tasks/create',(req,res)=>{
-    //midleware para a recepção dos dados da tarefa(task)
+    //midleware para a recepção dos dados da tarefa (task)
 
     //pegando os dados da request
-    const post_data = req.body;
+    const post_data= req.body;
 
-    //checar para ver se não estamos recebendo json vazia
-    if(post_data == undefined){
+    //checar se não estamos recebendo json vazia
+    if(post_data ==undefined){
         res.json(functions.response('Atenção','Sem dados de uma nova task',0,null))
         return;
     }
     const task = post_data.task;
     const status = post_data.status;
 
-    //inserindo a nova task
+    //Inserindo a nova task 
     connection.query('INSERT INTO tasks(task,status,created_at,update_at) VALUES(?,?,NOW(),NOW()',[task,status],(err,rows)=>{
         if(!err){
-            res.json('Sucesso','Task cadastrada no banco',rows.affectedRows,null)
+            res.json("Sucesso","Task cadastrada no banco",rows.affectedRows,null)
         }
         else{
             res.json(functions.response('Erro',err.message,0,null))
         }
     })
+   
 })
 
-//8 midleware para caso alguma rota não seja encontrada
+
+//rota para atualizar o texto de uma task
+//o texto da task sera enviado atraves do body
+
+app.put('/tasks/:id/update',(req,res)=>{
+    const id = req.params.id;
+    const post_data = req.body;
+    //checar se os dados estao vazios 
+    if(post_data==undefined){
+        res.json(functions.response('Atenção', ' Sem dados para uma nova task',0,null))
+        return
+    }
+    //checar se os dados são validos 
+    if(post_data.task == undefined || post_data.status == undefined){
+        res.json(functions.response('Atenção', 'Dados ivalidos',0,null))
+        return
+    }
+    //declara as variaveis para recpecionar as informações da task
+    const task = post_data.task;
+    const status = post_data.status;
+
+    //atualização dos dados 
+    connection.query('UPDATE tasks SETE task =?, upadate_at = NOW() WHERE id=?',(task,status,id),
+        (err,rows)=>{
+            if(!err){
+                if(!err){
+                    res.json(functions.response("Sucesso","Task atualizada",rows.affectedRows,null))
+                }else{
+                    res.json(functions.response("Atenção", "Task não encontrada",rows.affectedRows,null))
+                }
+            }else{
+                res.json(functions.response('Erro',err.message,0,null));
+            }
+        }
+    )
+
+})
+
+
+//mydlware para caso alguma rota não seja encontrada 
+
 app.use((req,res)=>{
-    res.json(functions.response('atenção', 'Rota não encontrada',0,null))
+    res.json(functions.response('Atenção','rota não encontrada',0,null))
 })
